5d2a9e8f9eed7d43aa5d989b663bc1fa6436fb32
HansN-92 <hanszonilsen@hotmail.com>
2026-06-02 15:06:13 +0200

    startet på bibliotek ekstra oppgave

---
+++ Bibliotek/Library.cs
@@ -0,0 +1,32 @@
+﻿namespace Bibliotek
+{
+    internal class Library
+    {
+        private List<Book> _books = new List<Book>();
+        private int _nextId = 1;
+
+        public void AddBook(Book book)
+        {
+            book.CreateBookId(_nextId++);
+            _books.Add(book);
+        }     
+        
+        public void RemoveBook(Book book)
+        {
+            _books.Remove(book);
+        }
+
+        public void SearchBooks()
+        {
+
+        }        
+        
+        public void ShowAllBooks()
+        {
+            foreach (Book book in _books)
+            {
+                Console.WriteLine($"{book}");
+            }
+        }
+    }
+}
---
+++ Bibliotek/Menu.cs
@@ -0,0 +1,10 @@
+﻿namespace Bibliotek
+{
+    internal class Menu
+    {
+        public static void ShowMain()
+        {
+            Library.ShowAllBooks();
+        }
+    }
+}
